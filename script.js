439a454acbfbc516735fed23e2a3bf3eeb23893b
Luca Sorace Stranck <strdjn@gmail.com>
2017-10-22 12:05:13 +0200

    V1.1
    - Removed worth thing from manifest.json
    - Improved performance by loading LIMITED_OR_NO_ADS page
    - Fixed ended message
    - Fixed wait for page loaded
    - Fixed check for demonetized videos
    - Fixed miss button press for request review
    - Added limit of button press
    - Added timeout for request review

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function getEditUrl(id) {
 }
 
 function getPageUrl(page) {
-    return "https://www.youtube.com/my_videos?o=U&pi=" + page;
+    return "https://www.youtube.com/my_videos?o=U&ls=LIMITED_OR_NO_ADS&pi=" + page;
 }
 
 function getParameterByName(name, url, char) {
@@ -52,72 +52,109 @@ function getVideoIds() {
 }
 
 function ended() {
-    return document.getElementsByClassName("yt-uix-button  yt-uix-pager-button yt-uix-button-toggled yt-uix-sessionlink yt-uix-button-default yt-uix-button-size-default") > 0;
+    return document.getElementsByClassName("yt-uix-button  yt-uix-pager-button yt-uix-button-toggled yt-uix-sessionlink yt-uix-button-default yt-uix-button-size-default").length > 0;
 }
 
 function dashboard(url) {
-    if (!ended()) {
-        console.log("Trying to get the video list");
-        if (document.getElementsByClassName("vm-video-title-content yt-uix-sessionlink").length <= 0)
-            setTimeout(function () { dashboard(url); }, 500); //check if the videos list is loaded
-        else {
-            var ids = getVideoIds();
-            if (ids.length > 0) nextId(ids, url, "pi", '?'); //go to the first id
-            else nextPage(url, "pi", '?'); //if no demonetized video, go to the next page
+    console.log("Wait for page loaded");
+    if (document.getElementsByClassName("yt-uix-button  yt-uix-pager-button yt-uix-sessionlink yt-uix-button-default yt-uix-button-size-default") == 0)
+        setTimeout(function () { dashboard(url); }, 250); //loop until page is loaded
+    else {
+        if (ended()) {
+            console.log("Trying to get the video list");
+            if (document.getElementsByClassName("vm-video-title-content yt-uix-sessionlink").length <= 0)
+                setTimeout(function () { dashboard(url); }, 500); //check if the videos list is loaded
+            else {
+                var ids = getVideoIds();
+                if (ids.length > 0) nextId(ids, url, "pi", '?'); //go to the first id
+                else nextPage(url, "pi", '?'); //if no demonetized video, go to the next page
+            }
+        } else {
+            url.replace("https://www.youtube.com/channel/UCmMWUz0QZ7WhIBx-1Dz-IGg");
+            alert("RLAFY - Done!\nRequested manual review for all of your videos!\n\nProgrammed by Stranck. Please consider to leave a star on my github :D");
         }
-    } else {
-        url.replace("https://www.youtube.com/channel/UCmMWUz0QZ7WhIBx-1Dz-IGg");
-        alert("RLAFY - Done!\nRequested manual review for all of your videos!\n\nProgrammed by Stranck. Please consider to leave a star on my github");
     }
 }
 
 //EDIT
 
 function checkIfDemonetized() {
-    console.log("Trying to get if demonetzied");
-    if (document.getElementsByClassName("vm-video-monetized vm-video-monetization-icon yt-uix-tooltip-reverse yt-uix-tooltip yt-sprite").length > 0)
-        return false; //if monetized icon already loaded, return false
-    if (document.getElementsByClassName("vm-video-monetization-limited-or-no-ads vm-video-monetization-icon yt-uix-tooltip-reverse yt-uix-tooltip yt-sprite").length > 0)
-        return !document.getElementsByClassName("ad-friendly-appeal-status-label").length > 0; //if demonetized sprite is loaded, return if there is the italic text saying you're already request the review
-    var v;
-    setTimeout(function () { v = checkIfDemonetized(); }, 250); //if no icon is loaded, retry in 250ms
-    return v;
+    return document.getElementsByClassName("vm-video-monetized vm-video-monetization-icon yt-uix-tooltip-reverse yt-uix-tooltip yt-sprite").length == 0 &&
+        document.getElementsByClassName("ad-friendly-appeal-status-label").length == 0;
 }
 
-function requestReview(operation) {
+function getLateralText(element, className) {
+    var father = element.parentElement.parentElement, ret;
+    try{
+        ret = father.getElementsByClassName(className)[0].innerHTML;
+    } catch (e) {
+        ret = father.innerHTML;;
+    }
+    return ret;
+}
+
+function getCheckbutton(className, secondClassName, text) {
+    var elements = document.getElementsByClassName(className), i = elements.length - 1;
+    while (i >= 0 && getLateralText(elements[i], secondClassName).localeCompare(text)) i--;
+    return elements[i];
+}
+
+function getButton(className, innerText) {
+    var elements = document.getElementsByClassName(className), i = 0;
+    while (i < elements.length && elements[i].children[0].innerHTML.localeCompare(innerText)) i++;
+    return elements[i];
+}
+
+function requestReview(operation, url, attempts) {
     var element;
     console.log("Click step: " + operation);
     switch (operation) {
         case 0: {
-            element = document.getElementsByClassName("yt-uix-button yt-uix-button-size-default yt-uix-button-primary ad-friendly-appeal-button")[0]; //Request manual review
-            break;
+            element = getButton("yt-uix-button yt-uix-button-size-default yt-uix-button-primary ad-friendly-appeal-button", "Request manual review\n");
+            break;  //Request manual review
         }
         case 1: {
-            element = document.getElementsByClassName("yt-uix-form-input-checkbox ng-pristine ng-untouched ng-valid")[8]; //I confirm that my video is suitable for all advertisers
-            break;
+            element = getCheckbutton("yt-uix-form-input-checkbox ng-pristine ng-untouched ng-valid", "ad-friendly-appeal-check-label", 
+                "I confirm that my video is <a href=\"  //support.google.com/youtube/answer/6162278?hl=en-GB&#10;\" target=\"_blank\">suitable for all advertisers</a>.\n");
+            break; //I confirm that my video is suitable for all advertisers
         }
         case 2: {
-            element = document.getElementsByClassName("yt-uix-button yt-uix-button-size-default yt-uix-button-primary")[4]; //Submit for review
-            break;
+            element = getButton("yt-uix-button yt-uix-button-size-default yt-uix-button-primary", "Submit for review\n");
+            break; //Submit for review
         }
     }
+    attempts++;
     if (element != undefined && element != null) { //if the button exist
-        element.click(); //click it
-        operation++; //go to next button
+        try {
+            element.click(); //click it
+            operation++; //go to next button
+        } catch (e) {
+            console.log(e);
+        }
     }
-    if(operation < 3) setTimeout(function () { requestReview(operation); }, 100);
+    if (attempts > 64) url.reload();
+        else if (operation < 3) setTimeout(function () { requestReview(operation, url, attempts); }, 100);
 }
 
-function edit(url) {
-    if (checkIfDemonetized()) {
-        console.log("Requesting manual review");
-        requestReview(0);
-    } else {
-        console.log("Already requested / not demonetized");
-        var ids = getParameterByName("ids", url.href, '#').split(','); //get all ids
-        if (ids[0].localeCompare("")) { //if there is one
-            nextId(ids, url, "page", '#'); //go to next id
-        } else nextPage(url, "page", '#'); //else go to next page
+function edit(url, attemps) {
+    console.log("Waiting for edit page load");
+    if (attemps > 3) url.reload();
+        else attemps++;
+    if (document.getElementsByClassName("vm-video-monetized vm-video-monetization-icon yt-uix-tooltip-reverse yt-uix-tooltip yt-sprite").length == 0 &&
+       document.getElementsByClassName("vm-video-monetization-limited-or-no-ads vm-video-monetization-icon yt-uix-tooltip-reverse yt-uix-tooltip yt-sprite").length == 0)
+           setTimeout(function () { edit(url, attemps); }, 250); //loop until a monetization sprite is loaded
+    else {
+        if (checkIfDemonetized()) {
+            console.log("Requesting manual review");
+            requestReview(0, url, 0);
+            setTimeout(function () { edit(url, attemps); }, 8000);
+        } else {
+            console.log("Already requested / not demonetized");
+            var ids = getParameterByName("ids", url.href, '#').split(','); //get all ids
+            if (ids[0].localeCompare("")) { //if there is one
+                nextId(ids, url, "page", '#'); //go to next id
+            } else nextPage(url, "page", '#'); //else go to next page
+        }
     }
 }
 
@@ -126,8 +163,9 @@ function edit(url) {
 var url = window.location;
 if (url.href.includes("my_videos")) {
     console.log("Detected my_videos page");
-    setTimeout(function () { dashboard(url); }, 500);
+    if (!url.href.includes("LIMITED_OR_NO_ADS")) url.replace(getPageUrl(1));
+        else setTimeout(function () { dashboard(url); }, 500);
 } else if (url.href.includes("edit")) {
     console.log("Detected edit page");
-    edit(url);
+    edit(url, 0);
 }
